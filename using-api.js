8ff8f6fff55a4562d233ff7c78eede44e86e4c01
clarammdantas <clara.moraesd@gmail.com>
2017-09-29 14:18:08 -0300

    usando mais funções da api e tentando criar gráficos com d3

--- using-api.js
+++ using-api.js
@@ -1,18 +1,22 @@
 angular.module('myApp', []).controller('Ctrl', ['$scope' , '$http', function($scope, $http) {
 
 	$scope.topTracks = [];
-	var apiKey = '9646370f4041cc9850f6ef70be921357';
+	$scope.topTracksPics = [];
 
+	var apiKey = '9646370f4041cc9850f6ef70be921357';
 
 	$scope.getUserInfo = function(user) {
 		var url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user='+ user +'&api_key=9646370f4041cc9850f6ef70be921357&format=json';
 		return $http.get(url).then(function(response) {
-			console.log(response);
 			$scope.topTracks = response.data.toptracks.track;
-
-		}); 
+			$scope.getTopTracksPic();
+		});
 	}
 
-		
-
+	$scope.getTopTracksPic = function() {
+		for (var i = 0; i < $scope.topTracks.length; i++) {
+			$scope.topTracksPics.push($scope.topTracks[i].image[2]['#text']);
+		};
+	};
+	
 }]);
